ad9619092af601c6b65e6a05a3f264dd6f3cf262
Ramkrishna <ramsah15483@gmail.com>
2026-09-06 10:53:12 +0545

    my week-2 commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function calculateSum() {
     let number1 = Number(document.getElementById("num1").value);
     let number2 = Number(document.getElementById("num2").value);
@@ -6,3 +7,4 @@ function calculateSum() {
 
     document.getElementById("result").innerHTML = "Sum = " + sum;
 }
+
